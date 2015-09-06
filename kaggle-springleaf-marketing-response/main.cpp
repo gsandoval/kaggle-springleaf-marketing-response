f48155cb5fcbe5da838f12df59f4728f1e87f4e0
Guillermo Sandoval <m@ker.gs>
2015-09-06 17:30:24 -0400

    Initial commit

---
+++ kaggle-springleaf-marketing-response/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  kaggle-springleaf-marketing-response
+//
+//  Created by Guillermo Alberto Sandoval Sanchez on 9/6/15.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
